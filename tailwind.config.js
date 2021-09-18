50da050b184c0180364f582d0a49bd70a00ac9ac
zaldi <fzlddd@gmail.com>
2021-09-18 10:50:34 +0700

    tailwind setup; colors, fonts

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "elemes-green": "#8BAC3E",
+        "elemes-light-green": "#A4B441",
+        "elemes-teal": "#40A2B1",
+        "elemes-blue": "#405EB6",
+        "elemes-magenta": "#B23F74",
+        "elemes-pizza": "#E6F3F5",
+        "elemes-kebab": "#EAEEFA",
+        "elemes-salmon": "#F9EEF3",
+        "elemes-cupcake": "#F0FEEB",
+        "elemes-doughnut": "#F3F7D9",
+      },
+    },
+    fontFamily: {
+      Rubik: ["Rubik, sans-serif"],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
